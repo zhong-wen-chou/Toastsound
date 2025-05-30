693547f7f1997df00ba091dcc396f9658b21a384
zhong-wen-chou <2251299086@qq.com>
2025-05-30 21:26:14 +0800

    更新UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,16 @@
 #include "mainwindow.h"
 #include "loadwindow.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    setWindowTitle("大吐司");
+    setWindowTitle("ToustSound");
+    setWindowIcon(QIcon(":/pictures/toust_icon.png")); // 设置图标
     setMinimumSize(800, 600);
+
     createWidgets(); // 创建控件
     setupLayout();   // 设置布局
-    connectSignals();// 连接信号
+    connectSignals(); // 连接信号
 }
 
 MainWindow::~MainWindow()
@@ -15,21 +18,19 @@ MainWindow::~MainWindow()
     delete scoreEditor;
 }
 
+// 创建控件
 void MainWindow::createWidgets()
 {
-    // 创建标题标签
-    QLabel *titleLabel = new QLabel("音乐编曲", this);
+    titleLabel = new QLabel("ToustSound", this);
     titleLabel->setAlignment(Qt::AlignCenter);
     titleLabel->setStyleSheet("font-size: 32px; font-weight: bold; color: #165DFF;");
 
-    // 创建副标题标签
-    QLabel *subtitleLabel = new QLabel("Created by 大吐司", this);
+    subtitleLabel = new QLabel("Created by Big Toust", this);
     subtitleLabel->setAlignment(Qt::AlignCenter);
     subtitleLabel->setStyleSheet("font-size: 16px; color: #555; margin-bottom: 40px;");
 
-    // 创建谱曲按钮
     composeButton = new QPushButton("谱曲", this);
-    composeButton->setMinimumSize(200, 60);
+    composeButton->setMinimumSize(30, 40);
     composeButton->setStyleSheet(
         "QPushButton {"
         "    background-color: #165DFF;"
@@ -43,9 +44,8 @@ void MainWindow::createWidgets()
         "}"
         );
 
-    // 创建读取按钮和帮助按钮
     loadButton = new QPushButton("读取", this);
-    loadButton->setMinimumSize(200, 60);
+    loadButton->setMinimumSize(30, 40);
     loadButton->setStyleSheet(
         "QPushButton {"
         "    background-color: #7B61FF;"
@@ -58,8 +58,9 @@ void MainWindow::createWidgets()
         "    background-color: #5A47CC;"
         "}"
         );
+
     helpButton = new QPushButton("帮助", this);
-    helpButton->setMinimumSize(200, 60);
+    helpButton->setMinimumSize(30, 40);
     helpButton->setStyleSheet(
         "QPushButton {"
         "    background-color: #FF7D00;"
@@ -73,63 +74,70 @@ void MainWindow::createWidgets()
         "}"
         );
 
-    // 创建底部标签
-    QLabel *versionLabel = new QLabel("© 大吐司 | 版本 1.0.0？maybe", this);
+    versionLabel = new QLabel("© 大吐司 | 版本 1.0.0？maybe", this);
     versionLabel->setAlignment(Qt::AlignCenter);
     versionLabel->setStyleSheet("color: #888; margin-top: 40px;");
-
-
 }
 
+// 设置布局和背景图
 void MainWindow::setupLayout()
 {
-    // 创建中央部件和垂直布局
+    // 创建中央部件
     QWidget *centralWidget = new QWidget(this);
+
+    // 设置背景图样式
+    centralWidget->setStyleSheet(
+        "background-image: url(:/pictures/mainbackgroud.png);"
+        "background-repeat: no-repeat;"
+        "background-position: center;"
+        "background-origin: content;"
+        );
+
+    // 创建垂直布局
     QVBoxLayout *layout = new QVBoxLayout(centralWidget);
     layout->setAlignment(Qt::AlignCenter);
     layout->setSpacing(20);
     layout->setContentsMargins(40, 40, 40, 40);
 
-    // 按顺序添加控件到布局
+    // 添加控件
     layout->addWidget(titleLabel);
     layout->addWidget(subtitleLabel);
     layout->addWidget(composeButton);
     layout->addWidget(loadButton);
     layout->addWidget(helpButton);
-    //layout->addItem(new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding));
     layout->addWidget(versionLabel);
+
     setCentralWidget(centralWidget);
 }
 
+// 连接信号
 void MainWindow::connectSignals()
 {
     connect(composeButton, &QPushButton::clicked, this, &MainWindow::onComposeButtonClicked);
-
-
     connect(loadButton, &QPushButton::clicked, this, &MainWindow::onLoadButtonClicked);
-
-
     connect(helpButton, &QPushButton::clicked, []() {
         qDebug() << "帮助按钮被点击";
     });
 }
 
+// 跳转到谱曲界面
 void MainWindow::onComposeButtonClicked()
 {
     qDebug() << "跳转到编曲界面";
-    hide(); // 隐藏主窗口
+    hide();
 
     if (!scoreEditor) {
-        scoreEditor = new ScoreEditor(this); // 父对象为主窗口
+        scoreEditor = new ScoreEditor(this);
         connect(scoreEditor, &ScoreEditor::exitRequested, this, [this]() {
             scoreEditor->hide();
-            show(); // 返回主窗口
+            show();
         });
     }
 
     scoreEditor->show();
 }
 
+// 跳转到读取界面
 void MainWindow::onLoadButtonClicked()
 {
     qDebug() << "跳转到读取界面";
@@ -137,7 +145,7 @@ void MainWindow::onLoadButtonClicked()
 
     if (!loadWindow) {
         loadWindow = new LoadWindow(this);
-        connect(loadWindow, &LoadWindow::destroyed, this, &MainWindow::show); // 返回时显示主窗口
+        connect(loadWindow, &LoadWindow::destroyed, this, &MainWindow::show);
     }
 
     loadWindow->show();
