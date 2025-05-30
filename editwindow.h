b4492831759906664b347264b5d415cd926ca645
zhong-wen-chou <2251299086@qq.com>
2025-05-30 16:55:50 +0800

    Update editwindow.h

--- editwindow.h
+++ editwindow.h
@@ -10,23 +10,38 @@
 #include "song.h"
 #include <QScrollArea>
 #include <QStackedWidget>
+#include <QTimer>
 
 class NoteCanvas : public QWidget {
     Q_OBJECT
 public:
+    const double pixelsPerMs = 0.1;
+    const qint64 viewDuration = 2*width() / pixelsPerMs; // 可视时间窗口
     explicit NoteCanvas(QWidget *parent = nullptr);
     void setNotes(const QVector<std::tuple<int, int>>& notes);
-    void addNote(int keyIndex, int position); // 添加音符到当前画布
+    void addNote(int keyIndex, qint64 startTime); // 添加音符到当前画布
+    qint64 currentTime() const; // 获取当前时间
+    void releaseNote(int keyIndex, qint64 endTime);
     void clearNotes(); // 清空当前画布的音符
     void updateCanvasSize(); // 更新画布大小
     int getLastNotePosition() const; // 获取最后一个音符的位置
+    struct NoteBlock {
+        int pitch;         // 如 "C4"
+        qint64 startTime;      // 单位：ms
+        qint64 endTime;        // 若正在演奏，则为 -1 或设为当前时间
+        bool active;           // 是否还在按下状态
+    };
 
 protected:
     void paintEvent(QPaintEvent *event) override;
 
 private:
-    QVector<std::tuple<int, int>> m_notes;
+    QVector<NoteBlock> NoteBlocks;
     int getNoteY(int keyIndex) const;
+    QElapsedTimer edittimer;
+    QTimer *refreshTimer;
+    //QMap<QString, int> pitchToY;  // 映射 pitch 到 y 轴
+    //qint64 viewWindowMs = 8000;   // 当前窗口显示过去 8 秒
     int lastNotePosition = 0; // 跟踪当前画布的音符位置
 };
 
@@ -39,7 +54,7 @@ public:
     ~EditWindow();
 
 private slots:
-    void onPianoKeyPressed(int keyIndex,int midinum);
+    void onPianoKeyPressed(int keyIndex);
     void onPianoKeyReleased(int keyIndex);
     void exitToMain();
     void startPlayback();
@@ -58,7 +73,6 @@ private:
     void setupLayout();
     void updateNoteVisual(int keyIndex, bool isPressed);
     void connectSignals();
-    void showTrackSettings(int trackIndex);
 
     PianoKeys *pianoKeys;
     QListWidget *trackList;
